60aaa62e684e2feaed20af1b72fc0a7cca5e09f0
Som Akudu <somakudu@gmail.com>
2023-04-30 23:35:47 +0100

    update valid anagram

--- valid_anagram_2.rb
+++ valid_anagram_2.rb
@@ -1,15 +1,18 @@
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
 def is_anagram(s, t)
-  return false if s.size != t.size
+  return false if s.length != t.length
+
   s_count = Hash.new(0)
   t_count = Hash.new(0)
 
-  s.chars.each_index do |i|
-    s_count[s[i]] += 1
-    t_count[t[i]] += 1
+  (0..s.length - 1).each do |i|
+      s_count[s[i]] += 1
+      t_count[t[i]] += 1
   end
 
-  p s_count
-  p t_count
+  s_count == t_count
 end
 
 # hashmap one loop version
