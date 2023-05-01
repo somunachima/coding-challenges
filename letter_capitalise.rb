6b9e57ff1738b3086fa3d0fa0faf403bad52cc35
Som Akudu <somakudu@gmail.com>
2023-05-01 21:32:11 +0100

    add letter capitalise from today

--- letter_capitalise.rb
+++ letter_capitalise.rb
@@ -1,3 +1,15 @@
+# latest version - May 2023
+def LetterCapitalize(str)
+  new = ""
+
+    str.chars.each_with_index do |char, idx|
+      str[idx - 1] == " " || idx == 0 ? new += char.tr("a-z", "A-Z") : new += char
+    end
+  return new
+end
+
+
+# old version - March 2023
 def LetterCapitalize(str)
   word = str.split
   # p word
