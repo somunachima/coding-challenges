16ba1e5a0a90789597d449383e1228b9ad5dae85
Som Akudu <somakudu@gmail.com>
2024-08-17 18:27:53 +0100

    Completed section 6

---
+++ Java/SectionSix/HelloWorld.java
@@ -0,0 +1,8 @@
+package ChapterFour;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        PrintMultiplication multiplyByFive = new PrintMultiplication();
+        multiplyByFive.printMultiplication();
+    }
+}
---
+++ Java/SectionSix/PrintMultiplication.java
@@ -0,0 +1,9 @@
+package ChapterFour;
+
+public class PrintMultiplication {
+    public static void printMultiplication() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%d x %d = %d", i, 5, i * 5).println();
+        }
+    }
+}
