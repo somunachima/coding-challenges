f3025bb8f6c0f7ad0870599babd270e70da497ff
Som Akudu <somakudu@gmail.com>
2024-08-23 21:21:56 +0100

    complete loop exercises

---
+++ Java/Section14/MyNumber.java
@@ -0,0 +1,41 @@
+package Section14;
+
+public class MyNumber {
+    int number;
+
+    MyNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+        for (int i = 2; i < number; i++) {
+            return number % i == 0;
+        }
+        return false;
+    }
+
+    public int sumUpToN() {
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int sumOfDivisors() {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public void printANumberTriangle() {
+        StringBuilder res = new StringBuilder(" ");
+        for (int i = 1; i <= number; i++) {
+            System.out.println(res.append(i).append(" "));
+        }
+    }
+}
---
+++ Java/Section14/MyNumberRunner.java
@@ -0,0 +1,13 @@
+package Section14;
+
+public class MyNumberRunner {
+    public static void main(String[] args) {
+        MyNumber number = new MyNumber(6);
+        System.out.println(number.isPrime());
+        int sum = number.sumUpToN();
+        System.out.println(sum);
+        int sumOfDivisors = number.sumOfDivisors();
+        System.out.println(sumOfDivisors);
+        number.printANumberTriangle();
+    }
+}
