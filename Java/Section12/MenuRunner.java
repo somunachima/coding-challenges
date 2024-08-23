d86526d872a5a91695fdc88eba329b26f4f2e10e
Som Akudu <somakudu@gmail.com>
2024-08-23 15:01:08 +0100

    add switch statements to MenuRunner

--- Java/Section12/MenuRunner.java
+++ Java/Section12/MenuRunner.java
@@ -11,26 +11,50 @@ public class MenuRunner {
         System.out.println("Enter Number2:");
         int number2 = scanner1.nextInt();
 
+        System.out.println("Select a number to choose operation:");
         System.out.println("1 - Add");
         System.out.println("2 - Subtract");
         System.out.println("3 - Divide");
         System.out.println("4 - Multiply");
         Scanner scanOperation = new Scanner(System.in);
         int operationNumber = scanOperation.nextInt();
-        System.out.println("Select a number to choose operation:");
 
         MenuCalculation menuCalculation = new MenuCalculation(number1, number2, operationNumber);
+//        performOperationUsingIfElse(operationNumber, menuCalculation);
+        performOperationUsingSwitch(operationNumber, menuCalculation);
+    }
+
+    private static void performOperationUsingSwitch(int operationNumber, MenuCalculation menuCalculation) {
+        switch (operationNumber) {
+            case 1:
+                menuCalculation.add();
+                break;
+            case 2:
+                menuCalculation.subtract();
+                break;
+            case 3:
+                menuCalculation.divide();
+                break;
+            case 4:
+                menuCalculation.multiply();
+                break;
+            default:
+                System.out.println("Invalid Operation Number");
+                break;
+        }
+    }
+
+    private static void performOperationUsingIfElse(int operationNumber, MenuCalculation menuCalculation) {
         if (operationNumber == 1) {
             menuCalculation.add();
-        }
-        if (operationNumber == 2) {
+        } else if (operationNumber == 2) {
             menuCalculation.subtract();
-        }
-        if (operationNumber == 3) {
+        } else if (operationNumber == 3) {
             menuCalculation.divide();
-        }
-        if (operationNumber == 4) {
+        } else if (operationNumber == 4) {
             menuCalculation.multiply();
+        } else {
+            System.out.println("Invalid Operation");
         }
     }
 }
